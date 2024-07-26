a68b12f7b43410c8929278dacd6639e70ae4e3dd
Solquarters <rom.kabucov@gmail.com>
2024-07-26 18:50:51 +0200

    first commit

---
+++ src/app/fruitlist/fruitlist.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-fruitlist',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './fruitlist.component.html',
+  styleUrl: './fruitlist.component.scss'
+})
+export class FruitlistComponent {
+
+}
